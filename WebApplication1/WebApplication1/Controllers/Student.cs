f8e59a246a9b01b800dfe10f73b6f8a6cf4d221a
Lavanth <176762554+Levanth93@users.noreply.github.com>
2025-07-11 11:27:48 +0530

    Add files via upload

---
+++ WebApplication1/WebApplication1/Controllers/Student.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace WebApplication1.Controllers
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; } 
+        public string dept { get; set; }
+
+    }
+}
